af660c966715ad2fa78d174d397abe1932b72c54
ziv <zivalt8@gmail.com>
2025-11-20 16:27:59 +0200

    add session and basic post weight functionality

--- weight/api/app.py
+++ weight/api/app.py
@@ -1,6 +1,37 @@
-import flask 
+from flask import Flask, request, session
+#from datetime import datetime
+from flask_sqlalchemy import  SQLAlchemy
+app = Flask(__name__)
+app.secret_key = "Temp"
 
-app = flask.Flask(__name__)
+
+@app.route( "/weight", methods = ["POST"])
+def post_weight():
+    direction = request.form["direction"]
+    truck = request.form["licence"]
+    weight = request.form["weight"]
+    containers = request.form["containers"]
+    unit = request.form["unit"]
+    force = request.form["force"]
+    produce = request.form["produce"]
+    # timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    handle_session(direction, truck) #handle the sessions
+
+    #todo add everything into db
+
+
+def handle_session(direction, truck):
+    if direction == "in" or direction == "none":
+        # generate session
+        session["truck_id"] = truck
+
+    elif direction == "out":
+        session.pop("truck_id", None)
+
+def get_containers_weight(containers):
+    data = {}
+    return data
 
 
 
