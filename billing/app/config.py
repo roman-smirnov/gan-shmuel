f5a7336481475fa71d66d82954cf7ee920a041a0
Anton <anton-nahhas@hotmail.com>
2025-11-21 13:42:07 +0200

    Add env variables configurations to config file in app direcotry

--- billing/app/config.py
+++ billing/app/config.py
@@ -1,3 +1,8 @@
 import os
+
 class Config:
    DEBUG = os.environ.get("DEBUG", "false").lower() == "true"
+   DB_HOST = os.getenv('DB_HOST', 'localhost')
+   DB_USER = os.getenv('DB_USER', 'root')
+   DB_PASSWORD = os.getenv('DB_PASSWORD', 'password')
+   DB_NAME = os.getenv('DB_NAME', 'billdb')
