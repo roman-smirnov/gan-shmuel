09bd9b5f8bc42a07fe01af44f3ea8f8398f9c572
Anton <anton-nahhas@hotmail.com>
2025-11-23 11:33:11 +0200

    Add new env variable for weight base url

--- billing/app/config.py
+++ billing/app/config.py
@@ -3,8 +3,11 @@ import os
 class Config:
    DEBUG = os.environ.get("DEBUG", "false").lower() == "true"
 
+   WEIGHT_BASE_UL = os.environ["WEIGHT_BASE_URL"]
+
    DB_HOST = os.environ['DB_HOST']
    DB_USER = os.environ['DB_USER']
    DB_PASSWORD = os.environ['DB_PASSWORD']
    DB_NAME = os.environ['DB_NAME']
    DB_PORT = os.environ['DB_PORT']
+   
