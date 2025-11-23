6c4195f75505e7b8baf95102013540611fb6f2a8
Anton <anton-nahhas@hotmail.com>
2025-11-23 11:29:37 +0200

    Replace all required env variables to mandatory lookups

--- billing/app/config.py
+++ billing/app/config.py
@@ -3,8 +3,8 @@ import os
 class Config:
    DEBUG = os.environ.get("DEBUG", "false").lower() == "true"
 
-   DB_HOST = os.getenv('DB_HOST')
-   DB_USER = os.getenv('DB_USER')
-   DB_PASSWORD = os.getenv('DB_PASSWORD')
-   DB_NAME = os.getenv('DB_NAME')
-   DB_PORT = os.getenv('DB_PORT')
+   DB_HOST = os.environ['DB_HOST']
+   DB_USER = os.environ['DB_USER']
+   DB_PASSWORD = os.environ['DB_PASSWORD']
+   DB_NAME = os.environ['DB_NAME']
+   DB_PORT = os.environ['DB_PORT']
