94ab32248108e6ddb41b571d087e80c30ae7babb
Daniel Shimon <danielsh200@gmail.com>
2025-11-24 00:14:48 +0200

    Refactor Dockerfile and app.py to integrate utils module for better code organization and maintainability

--- weight/api/app.py
+++ weight/api/app.py
@@ -1,62 +1,28 @@
 from datetime import datetime, timezone
-from flask import Flask, Response, request, session
+from flask import Flask, Response, request
 from flask_sqlalchemy import SQLAlchemy
 import secrets
 import os
 import json
-
-
+import utils
+    
 
 # Initialize Flask app and SQLAlchemy
 app = Flask(__name__)
-app.secret_key = secrets.token_hex(6)  # generate a random 12 characters in hexadecimal for secret key
+app.secret_key = secrets.token_hex(
+    6
+)  # generate a random 12 characters in hexadecimal for secret key
 
 # Configure the database connection
 db_user = os.getenv("MYSQL_USER")
 db_pass = os.getenv("MYSQL_PASSWORD")
 db_name = os.getenv("MYSQL_DATABASE")
-app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+pymysql://{db_user}:{db_pass}@weight-db:3306/{db_name}"
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    f"mysql+pymysql://{db_user}:{db_pass}@weight-db:3306/{db_name}"
+)
 app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {"pool_pre_ping": True}
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
-
-def convert_kg_to_lbs(kg_num):
-    return int(float(kg_num)*2.20)
-
-def convert_lbs_to_kg(lbs_num):
-    return int(float(lbs_num) / 2.20)
-
-def calc_containers_weight(containers):
-    #todo: take into consideration weight unit differences
-    #the function receives a list(string separated by ",") of containers
-    #the function return the total weight of the containers or na if there was an issue
-    total_weight = 0
-    try:
-        id_list = [int(i) for i in containers.split(",")]  # creates a list of id from the containers
-        results = (db.session.query(Containers_registered.weight, Containers_registered.unit).filter
-                   (Containers_registered.container_id.in_(id_list)).all())  # get list of tuples with all container weight
-        if len(results) < len(id_list):
-            return None
-        for value in results:  # loops on all list
-            if value[1] == "kg":
-                total_weight = total_weight + value[0]
-            else:
-                total_weight = total_weight + convert_lbs_to_kg(value[0])
-        return total_weight
-
-    except: #except will raise in case the container wasn't found  and return na
-        return None
-
-
-def calc_neto_fruit(bruto_weight,truckTara, containers):
-    #this functions receives a bruto weight, truck tara and a list(string separated by ",") of containers
-    #and returns the neto weight by the following calculation neto = brutu - truck tara - containers_tara
-    container_weight = calc_containers_weight(containers)
-    if container_weight:
-        return bruto_weight - (int(truckTara) + container_weight)
-
-    else:
-        return None
 
 
 # Define database models
@@ -82,29 +48,10 @@ class Containers_registered(db.Model):
     weight = db.Column(db.Integer)
     unit = db.Column(db.String(10))
 
-
-# helpter functions for db queries
-
-
-# convert string to datetime
-def str_to_datetime(ts):
-    return datetime.strptime(ts, "%Y%m%d%H%M%S")
-
-def get_query_transactions(from_date=None, to_date=None, direction_filter=None, item_filter=None, truck_filter=None):
-    query = Transactions.query
-    if from_date:
-        query = query.filter(Transactions.datetime >= from_date)
-    if to_date:
-        query = query.filter(Transactions.datetime <= to_date)
-    if direction_filter in ["in", "out"]:
-        query = query.filter(Transactions.direction == direction_filter)
-    if item_filter:
-        query = query.filter(Transactions.produce == item_filter)
-    if truck_filter:
-        query = query.filter(Transactions.truck == truck_filter)
-    return query.all()
-
-
+# inject dependencies to utils
+utils.db = db
+utils.Transactions = Transactions
+utils.Containers_registered = Containers_registered
 
 
 # endpoint definitions
@@ -121,13 +68,13 @@ def get_weight():
     raw_to = request.args.get("to")
     direction = request.args.get("filter")
 
-
-
     # Parse dates if provided
-    from_date = str_to_datetime(raw_from) if raw_from else None
-    to_date = str_to_datetime(raw_to) if raw_to else None
+    from_date = utils.str_to_datetime(raw_from) if raw_from else None
+    to_date = utils.str_to_datetime(raw_to) if raw_to else None
 
-    relevant_transactions = get_query_transactions(from_date, to_date, direction, None, None)
+    relevant_transactions = utils.get_query_transactions(
+        from_date, to_date, direction, None, None
+    )
     results = []
     for transaction in relevant_transactions:
         results.append(
@@ -142,41 +89,15 @@ def get_weight():
         )
     return {"results": results}
 
-def update_row(old_row, new_row):
-    old_row.neto = new_row.neto
-    old_row.bruto = new_row.bruto
-    old_row.containers = new_row.containers
-    old_row.truck = new_row.truck
-    old_row.produce = new_row.produce
-    db.session.commit()
-
-    return 1
-
-def verbose(row):
-    #the function receives a transaction row
-    #the functions return json for post weight return value
-    if row.direction == "out":
-        return {
-            "id": row.id,
-            "truck": row.truck,
-            "bruto": row.bruto,
-            "truckTara": row.truckTara,
-            "neto": row.neto
-        }
-    else:
-        return {
-            "id": row.id,
-            "truck": row.truck,
-            "bruto": row.bruto
-        }
 
 @app.route("/weight", methods=["POST"])
 def post_weight():
-
     data = request.form.to_dict()
-    #last_row = Transactions.query.order_by(Transactions.id.desc()).first()
+    # last_row = Transactions.query.order_by(Transactions.id.desc()).first()
     last_row = None
-    rows = get_query_transactions(None, None, None, None, data["truck"]) #get the last transaction of the truck
+    rows = utils.get_query_transactions(
+        None, None, None, None, data["truck"]
+    )  # get the last transaction of the truck
     if rows:
         last_row = rows[-1]
 
@@ -188,31 +109,38 @@ def post_weight():
     new_row.truck = int(data["truck"])
     new_row.containers = data["containers"]
     new_row.truckTara = None
-    handle_session(new_row, data["direction"], data["truck"])  # handle the sessions
-    if last_row: #check if the last row exist
-
+    utils.handle_session(new_row, data["direction"], data["truck"])  # handle the sessions
+    if last_row:  # check if the last row exist
         if last_row.direction == "in" and new_row.direction == "out":
-            containers_weight = calc_containers_weight(new_row.containers)
+            containers_weight = utils.calc_containers_weight(new_row.containers)
             if containers_weight:
-                new_row.truckTara = new_row.bruto - calc_containers_weight(new_row.containers)
-                neto = calc_neto_fruit(int(last_row.bruto), new_row.truckTara, data["containers"])
+                new_row.truckTara = new_row.bruto - utils.calc_containers_weight(
+                    new_row.containers
+                )
+                neto = utils.calc_neto_fruit(
+                    int(last_row.bruto), new_row.truckTara, data["containers"]
+                )
                 new_row.neto = neto
 
-        if (( last_row.direction == "in" and last_row.direction == new_row.direction )
-            or (last_row.direction == "out" and last_row.direction == new_row.direction)):
-
+        if (last_row.direction == "in" and last_row.direction == new_row.direction) or (
+            last_row.direction == "out" and last_row.direction == new_row.direction
+        ):
             if not data["force"]:
-                raise Exception(f"truck already {last_row.direction} use force True to update")
+                raise Exception(
+                    f"truck already {last_row.direction} use force True to update"
+                )
             elif data["force"] == "True":
-                update_row(last_row,new_row )
-                return verbose(last_row)
+                utils.update_row(last_row, new_row)
+                return utils.verbose(last_row)
             else:
-                raise Exception(f"truck already {last_row.direction} use force True to update")
+                raise Exception(
+                    f"truck already {last_row.direction} use force True to update"
+                )
 
     db.session.add(new_row)
     db.session.commit()
     #
-    return verbose(new_row)
+    return utils.verbose(new_row)
 
 
 @app.route("/item/<item_id>", methods=["GET"])
@@ -220,22 +148,26 @@ def get_item(item_id):
     raw_from = request.args.get("from")
     raw_to = request.args.get("to")
 
-    # return 404 if item isnt availble 
-    if len(get_query_transactions(None,None,None,item_id,None)) == 0 :
+    # return 404 if item isnt availble
+    if len(utils.get_query_transactions(None, None, None, item_id, None)) == 0:
         return Response("Item not found", status=404)
-    
+
     # handle date range
     if not raw_from:
-        from_date = datetime.now().replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+        from_date = datetime.now().replace(
+            day=1, hour=0, minute=0, second=0, microsecond=0
+        )
     else:
-        from_date = str_to_datetime(raw_from)
+        from_date = utils.str_to_datetime(raw_from)
     if not raw_to:
         to_date = datetime.now()
     else:
-        to_date = str_to_datetime(raw_to)
+        to_date = utils.str_to_datetime(raw_to)
     # query transactions by item
 
-    relevent_transactions = get_query_transactions(from_date, to_date, None, item_id, None)
+    relevent_transactions = utils.get_query_transactions(
+        from_date, to_date, None, item_id, None
+    )
     results = []
     for transaction in relevent_transactions:
         results.append(
@@ -246,6 +178,7 @@ def get_item(item_id):
             }
         )
     return results
+
 
 @app.route("/batch-weight", methods=["POST"])
 def batch_weight():
@@ -259,8 +192,12 @@ def batch_weight():
                 unit = header[1]
                 for line in lines[1:]:
                     cid, weight = line.strip().split(",")
-                    db.session.add(Containers_registered(container_id=cid, weight=weight, unit=unit))
-                    
+                    db.session.add(
+                        Containers_registered(
+                            container_id=cid, weight=weight, unit=unit
+                        )
+                    )
+
             db.session.commit()
             return Response("Batch processed successfully", status=200)
         case "json":
@@ -270,41 +207,31 @@ def batch_weight():
                 cid = entry["id"]
                 weight = entry["weight"]
                 unit = entry["unit"]
-                db.session.add(Containers_registered(container_id=cid, weight=weight, unit=unit))
+                db.session.add(
+                    Containers_registered(container_id=cid, weight=weight, unit=unit)
+                )
             db.session.commit()
             return Response("Batch processed successfully", status=200)
-        
+
         case _:
             return Response("Unsupported file format", status=400)
-        
+
 
 @app.route("/unknown", methods=["GET"])
 def unknown():
     result = []
-    unknown_containers = (db.session.query(Containers_registered.container_id)
-                          .filter(Containers_registered.weight == "").all())
+    unknown_containers = (
+        db.session.query(Containers_registered.container_id)
+        .filter(Containers_registered.weight == "")
+        .all()
+    )
     for container in unknown_containers:
         result.append(container.container_id)
     if not result:
-        return Response("No unknown containers found", status=204, mimetype="text/plain") 
+        return Response(
+            "No unknown containers found", status=204, mimetype="text/plain"
+        )
     return Response(", ".join(result) + ",", status=200, mimetype="text/plain")
-    
-
-
-def handle_session(new_row,direction, truck):
-    if direction == "in" or direction == "none":
-        # generate session
-        rand_num = secrets.randbelow(2000000000)#creates a random number for the
-        session[truck] = rand_num
-        new_row.session_id = rand_num
-
-    elif direction == "out":
-        rows = get_query_transactions(None, None, None, None, truck)  # get the last transaction of the truck
-        if rows:
-            last_row = rows[-1]
-            session_id = last_row.session_id
-            new_row.session_id = session_id
-            session.pop(truck, None)
 
 
 if __name__ == "__main__":
---
+++ weight/api/utils.py
@@ -0,0 +1,138 @@
+import secrets
+from datetime import datetime 
+from flask import session
+
+# will be injected from app.py
+db = None
+Transactions = None
+Containers_registered = None
+
+
+
+# ---
+# calculate helper functions
+# ---
+def calc_containers_weight(containers):
+    # todo: take into consideration weight unit differences
+    # the function receives a list(string separated by ",") of containers
+    # the function return the total weight of the containers or na if there was an issue
+
+    total_weight = 0
+    try:
+        id_list = [
+            int(i) for i in containers.split(",")
+        ]  # creates a list of id from the containers
+        results = (
+            db.session.query(Containers_registered.weight, Containers_registered.unit)
+            .filter(Containers_registered.container_id.in_(id_list))
+            .all()
+        )  # get list of tuples with all container weight
+        if len(results) < len(id_list):
+            return None
+        for value in results:  # loops on all list
+            if value[1] == "kg":
+                total_weight = total_weight + value[0]
+            else:
+                total_weight = total_weight + convert_lbs_to_kg(value[0])
+        return total_weight
+
+    except:  # except will raise in case the container wasn't found  and return na
+        return None
+
+
+def calc_neto_fruit(bruto_weight, truckTara, containers):
+    # this functions receives a bruto weight, truck tara and a list(string separated by ",") of containers
+    # and returns the neto weight by the following calculation neto = brutu - truck tara - containers_tara
+    container_weight = calc_containers_weight(containers)
+    if container_weight:
+        return bruto_weight - (int(truckTara) + container_weight)
+
+    else:
+        return None
+
+
+def convert_kg_to_lbs(kg_num):
+    return int(float(kg_num) * 2.20)
+
+
+def convert_lbs_to_kg(lbs_num):
+    return int(float(lbs_num) / 2.20)
+
+
+# ---
+# query helper functions
+# ---
+
+
+# convert string to datetime
+def str_to_datetime(ts):
+    return datetime.strptime(ts, "%Y%m%d%H%M%S")
+
+
+def get_query_transactions(
+    from_date=None,
+    to_date=None,
+    direction_filter=None,
+    item_filter=None,
+    truck_filter=None,
+):
+    query = Transactions.query
+    if from_date:
+        query = query.filter(Transactions.datetime >= from_date)
+    if to_date:
+        query = query.filter(Transactions.datetime <= to_date)
+    if direction_filter in ["in", "out"]:
+        query = query.filter(Transactions.direction == direction_filter)
+    if item_filter:
+        query = query.filter(Transactions.produce == item_filter)
+    if truck_filter:
+        query = query.filter(Transactions.truck == truck_filter)
+    return query.all()
+
+
+# ---
+# other helper functions
+# ---
+def update_row(old_row, new_row):
+    old_row.neto = new_row.neto
+    old_row.bruto = new_row.bruto
+    old_row.containers = new_row.containers
+    old_row.truck = new_row.truck
+    old_row.produce = new_row.produce
+    db.session.commit()
+
+    return 1
+
+
+def verbose(row):
+    # the function receives a transaction row
+    # the functions return json for post weight return value
+    if row.direction == "out":
+        return {
+            "id": row.id,
+            "truck": row.truck,
+            "bruto": row.bruto,
+            "truckTara": row.truckTara,
+            "neto": row.neto,
+        }
+    else:
+        return {"id": row.id, "truck": row.truck, "bruto": row.bruto}
+
+
+def handle_session(new_row, direction, truck):
+
+    if direction == "in" or direction == "none":
+        # generate session
+        rand_num = secrets.randbelow(2000000000)  # creates a random number for the
+        session[truck] = rand_num
+        new_row.session_id = rand_num
+
+    elif direction == "out":
+        rows = get_query_transactions(
+            None, None, None, None, truck
+        )  # get the last transaction of the truck
+        if rows:
+            last_row = rows[-1]
+            session_id = last_row.session_id
+            new_row.session_id = session_id
+            session.pop(truck, None)
