d5216ce516052210e4baaea1e59e3586991cec79
Anton <anton-nahhas@hotmail.com>
2025-11-23 11:11:34 +0200

    Add

--- billing/app/config.py
+++ billing/app/config.py
@@ -2,8 +2,9 @@ import os
 
 class Config:
    DEBUG = os.environ.get("DEBUG", "false").lower() == "true"
-   DB_HOST = os.getenv('DB_HOST', 'localhost')
-   DB_USER = os.getenv('DB_USER', 'root')
-   DB_PASSWORD = os.getenv('DB_PASSWORD', 'password')
-   DB_NAME = os.getenv('DB_NAME', 'billdb')
-   DB_PORT = os.getenv('DB_PORT', '3306')
+
+   DB_HOST = os.getenv('DB_HOST')
+   DB_USER = os.getenv('DB_USER')
+   DB_PASSWORD = os.getenv('DB_PASSWORD')
+   DB_NAME = os.getenv('DB_NAME')
+   DB_PORT = os.getenv('DB_PORT')
