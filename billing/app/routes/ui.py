f890c6bfe66bf5ca673362dd10dfd0c275802366
Maayan Efrati <maayanefrati70@gmail.com>
2025-11-24 14:46:58 +0200

    Update import get_provider_by_name and check if the name exists

--- billing/app/routes/ui.py
+++ billing/app/routes/ui.py
@@ -1,14 +1,11 @@
 from flask import Blueprint, render_template, request
-from app.models.provider import get_all_providers, create_provider
-
-ui_bp = Blueprint("ui", __name__)
+from app.models.provider import create_provider, get_all_providers, get_provider_by_name
 
 @ui_bp.route("/", methods=["GET", "POST"])
 def home():
     error_message = None
     success_message = None
 
-    # בשלב הזה כבר נטפל גם ב-POST של providers
     if request.method == "POST":
         form_type = request.form.get("form_type")
 
@@ -18,13 +15,18 @@ def home():
             if not name:
                 error_message = "Provider name is required."
             else:
-                try:
-                    provider_id = create_provider(name)
-                    success_message = f"Provider created with id {provider_id}"
-                except Exception:
-                    error_message = "Failed to create provider."
+                #check if name already exixt;
+                existing = get_provider_by_name(name)
+                if existing:
+                    error_message = "This provider name already exists."
+                else:
+                    try:
+                        provider_id = create_provider(name)
+                        success_message = f"Provider created with id {provider_id}"
+                    except Exception:
+                        error_message = "Failed to create provider."
 
-    # תמיד נביא את רשימת הספקים
+    # We will always re-fetch the list of providers:
     providers = get_all_providers()
 
     return render_template(
@@ -33,4 +35,3 @@ def home():
         error_message=error_message,
         success_message=success_message,
     )
-
