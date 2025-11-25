62a88b11b445b680f994e557f9083542bcb83d56
Maayan Efrati <maayanefrati70@gmail.com>
2025-11-25 09:37:54 +0200

    update html, init

--- billing/app/__init__.py
+++ billing/app/__init__.py
@@ -4,22 +4,22 @@ from .routes.health import health_bp
 from .routes.provider import providers_bp
 from .routes.truck import trucks_bp
 from .routes.rates import rates_bp
-from .routes.provider_ui import ui_bp
-from .routes.ui_rates import ui_rates_bp
-'''from .routes.providers import providers_bp
-from .routes.bills import bills_bp'''
+from .routes.provider_ui import ui_provider_bp
+from .routes.rates_ui import ui_rates_bp
+# from .routes.bills import bills_bp  # still commented
+
 
 def create_app():
     app = Flask(__name__)
-
     app.config.from_object(Config)
 
     app.register_blueprint(health_bp)
     app.register_blueprint(providers_bp)
     app.register_blueprint(trucks_bp)
     app.register_blueprint(rates_bp)
-    app.register_blueprint(ui_bp)
+    app.register_blueprint(ui_provider_bp)
     app.register_blueprint(ui_rates_bp)
     # app.register_blueprint(bills_bp)
 
     return app
+
